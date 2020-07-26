c2d68efc276e1af455ee47d021f7ad6a33ad988d
MoriyaShiine <moriyashiine@gmail.com>
2020-07-26 15:21:45 -0500

    init stuff

---
+++ src/main/java/moriyashiine/extraorigins/common/ExtraOrigins.java
@@ -0,0 +1,9 @@
+package moriyashiine.extraorigins.common;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExtraOrigins implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
