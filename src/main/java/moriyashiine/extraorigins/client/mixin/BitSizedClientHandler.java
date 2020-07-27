6ecab03fc2f50adccae417fdc7f42cc17beb03dc
MoriyaShiine <moriyashiine@gmail.com>
2020-07-26 22:06:25 -0500

    fix shadows

--- src/main/java/moriyashiine/extraorigins/client/mixin/BitSizedClientHandler.java
+++ src/main/java/moriyashiine/extraorigins/client/mixin/BitSizedClientHandler.java
@@ -4,11 +4,16 @@ import moriyashiine.extraorigins.common.registry.EOPowers;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
@@ -20,6 +25,19 @@ public class BitSizedClientHandler
 		if (EOPowers.BITE_SIZED.isActive(abstractClientPlayerEntity))
 		{
 			matrixStack.scale(0.5f, 0.5f, 0.5f);
+		}
+	}
+	
+	@Mixin(EntityRenderDispatcher.class)
+	private static class Shadow
+	{
+		@ModifyVariable(method = {"renderShadow"}, at = @At("HEAD"), index = 6)
+		private static float renderShadow(float radius, MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity, float opacity, float tickDelta, WorldView world) {
+			if (EOPowers.BITE_SIZED.isActive(entity))
+			{
+				return radius * 0.25f;
+			}
+			return radius;
 		}
 	}
 }
