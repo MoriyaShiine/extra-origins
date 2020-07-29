52cc848db9f5657a7195b24a074ee91e5c6adad7
MoriyaShiine <moriyashiine@gmail.com>
2020-07-28 21:28:51 -0500

    reformat

--- src/main/java/moriyashiine/extraorigins/common/interfaces/BiteSizedAccessor.java
+++ src/main/java/moriyashiine/extraorigins/common/interfaces/BiteSizedAccessor.java
@@ -1,7 +1,6 @@
 package moriyashiine.extraorigins.common.interfaces;
 
-public interface BiteSizedAccessor
-{
+public interface BiteSizedAccessor {
 	boolean getBiteSized();
 	
 	void setBiteSized(boolean biteSized);
