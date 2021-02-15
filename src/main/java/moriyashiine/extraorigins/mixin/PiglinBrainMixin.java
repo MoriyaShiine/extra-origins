029d587df9231f7d227493743af81203c28d9388
MoriyaShiine <moriyashiine@gmail.com>
2021-02-14 19:56:34 -0600

    fix #29

--- src/main/java/moriyashiine/extraorigins/mixin/PiglinBrainMixin.java
+++ src/main/java/moriyashiine/extraorigins/mixin/PiglinBrainMixin.java
@@ -3,9 +3,11 @@ package moriyashiine.extraorigins.mixin;
 import moriyashiine.extraorigins.common.registry.EOPowers;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.PiglinBrain;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PiglinBrain.class)
@@ -14,6 +16,13 @@ public abstract class PiglinBrainMixin {
 	private static void wearsGoldArmor(LivingEntity target, CallbackInfoReturnable<Boolean> callbackInfo) {
 		if (EOPowers.ALL_THAT_GLITTERS.isActive(target)) {
 			callbackInfo.setReturnValue(true);
+		}
+	}
+	
+	@Inject(method = "onGuardedBlockInteracted", at = @At("HEAD"), cancellable = true)
+	private static void onGuardedBlockInteracted(PlayerEntity player, boolean blockOpen, CallbackInfo callbackInfo) {
+		if (EOPowers.ALL_THAT_GLITTERS.isActive(player)) {
+			callbackInfo.cancel();
 		}
 	}
 }
