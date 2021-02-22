497ea34cbf11165b246b7d4c36c02d2b44ddb59a
MoriyaShiine <moriyashiine@gmail.com>
2021-02-22 08:44:22 -0600

    update german translation

--- src/main/java/moriyashiine/extraorigins/mixin/EntityMixin.java
+++ src/main/java/moriyashiine/extraorigins/mixin/EntityMixin.java
@@ -2,9 +2,7 @@ package moriyashiine.extraorigins.mixin;
 
 import moriyashiine.extraorigins.common.registry.EOPowers;
 import net.minecraft.entity.Entity;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
