becd02e3cdf307efc200c4952903746fd25ea767
MoriyaShiine <moriyashiine@gmail.com>
2021-03-02 12:55:19 -0600

    clean, also the pr fixes #68

--- src/main/java/moriyashiine/extraorigins/common/registry/EOScaleTypes.java
+++ src/main/java/moriyashiine/extraorigins/common/registry/EOScaleTypes.java
@@ -1,7 +1,5 @@
 package moriyashiine.extraorigins.common.registry;
 
-import java.util.Map;
-
 import moriyashiine.extraorigins.common.ExtraOrigins;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
@@ -9,6 +7,8 @@ import virtuoel.pehkui.api.ScaleData;
 import virtuoel.pehkui.api.ScaleModifier;
 import virtuoel.pehkui.api.ScaleRegistries;
 import virtuoel.pehkui.api.ScaleType;
+
+import java.util.Map;
 
 public class EOScaleTypes {
 	public static final ScaleModifier MODIFY_SIZE_MODIFIER = register(ScaleRegistries.SCALE_MODIFIERS, "modify_size", new ScaleModifier() {
@@ -18,30 +18,26 @@ public class EOScaleTypes {
 		}
 	});
 	
+	private static final ScaleType[] MODIFIED_SIZE_TYPES = {ScaleType.WIDTH, ScaleType.HEIGHT, ScaleType.DROPS};
+	
 	public static final ScaleType MODIFY_SIZE_TYPE = register(ScaleRegistries.SCALE_TYPES, "modify_size", ScaleType.Builder.create().build());
 	
 	private static <T> T register(Map<Identifier, T> registry, String name, T entry) {
 		return ScaleRegistries.register(registry, new Identifier(ExtraOrigins.MODID, name), entry);
 	}
 	
-	private static final ScaleType[] MODIFIED_SIZE_TYPES = { ScaleType.WIDTH, ScaleType.HEIGHT, ScaleType.DROPS };
-	
 	public static void init() {
 		for (ScaleType type : MODIFIED_SIZE_TYPES) {
 			type.getDefaultBaseValueModifiers().add(MODIFY_SIZE_MODIFIER);
 		}
-		
-		MODIFY_SIZE_TYPE.getScaleChangedEvent().register(s -> {
-			Entity e = s.getEntity();
-			
-			if (e != null) {
-				boolean onGround = e.isOnGround();
-				e.calculateDimensions();
-				e.setOnGround(onGround);
-				
+		MODIFY_SIZE_TYPE.getScaleChangedEvent().register(event -> {
+			Entity entity = event.getEntity();
+			if (entity != null) {
+				boolean onGround = entity.isOnGround();
+				entity.calculateDimensions();
+				entity.setOnGround(onGround);
 				for (ScaleType scaleType : ScaleRegistries.SCALE_TYPES.values()) {
-					ScaleData data = scaleType.getScaleData(e);
-					
+					ScaleData data = scaleType.getScaleData(entity);
 					if (data.getBaseValueModifiers().contains(MODIFY_SIZE_MODIFIER)) {
 						data.markForSync(true);
 					}
