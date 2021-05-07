b998fe8b26555a4f3e4dd19fd4a71e95ce1cc80f
MoriyaShiine <moriyashiine@gmail.com>
2021-05-06 19:43:24 -0500

    update to pehkui 2.1.0

--- src/main/java/moriyashiine/extraorigins/common/registry/EOScaleTypes.java
+++ src/main/java/moriyashiine/extraorigins/common/registry/EOScaleTypes.java
@@ -2,22 +2,17 @@ package moriyashiine.extraorigins.common.registry;
 
 import moriyashiine.extraorigins.common.ExtraOrigins;
 import net.minecraft.util.Identifier;
-import virtuoel.pehkui.api.ScaleData;
 import virtuoel.pehkui.api.ScaleModifier;
 import virtuoel.pehkui.api.ScaleRegistries;
 import virtuoel.pehkui.api.ScaleType;
+import virtuoel.pehkui.api.TypedScaleModifier;
 
 import java.util.Map;
 
 public class EOScaleTypes {
 	private static final ScaleType[] MODIFY_SIZE_TYPES = {ScaleType.WIDTH, ScaleType.HEIGHT, ScaleType.DROPS};
 	
-	public static final ScaleModifier MODIFY_SIZE_MODIFIER = register(ScaleRegistries.SCALE_MODIFIERS, new ScaleModifier() {
-		@Override
-		public float modifyScale(final ScaleData scaleData, float modifiedScale, final float delta) {
-			return MODIFY_SIZE_TYPE.getScaleData(scaleData.getEntity()).getScale(delta) * modifiedScale;
-		}
-	});
+	public static final ScaleModifier MODIFY_SIZE_MODIFIER = register(ScaleRegistries.SCALE_MODIFIERS, new TypedScaleModifier(() -> EOScaleTypes.MODIFY_SIZE_TYPE));
 	
 	public static final ScaleType MODIFY_SIZE_TYPE = register(ScaleRegistries.SCALE_TYPES, ScaleType.Builder.create().addDependentModifier(MODIFY_SIZE_MODIFIER).affectsDimensions().build());
 	
