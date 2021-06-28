a444c70f513fa706edf11e0fc1758951201a0e0f
MoriyaShiine <moriyashiine@gmail.com>
2021-06-27 20:40:29 -0500

    fix mob neutrality power not working if player looks at enderman

---
+++ src/main/java/moriyashiine/extraorigins/mixin/EndermanEntityMixin.java
@@ -0,0 +1,25 @@
+package moriyashiine.extraorigins.mixin;
+
+import io.github.apace100.apoli.component.PowerHolderComponent;
+import moriyashiine.extraorigins.common.power.MobNeutralityPower;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EndermanEntity.class)
+public abstract class EndermanEntityMixin {
+	@Inject(method = "isPlayerStaring", at = @At("RETURN"), cancellable = true)
+	private void isPlayerStaring(PlayerEntity player, CallbackInfoReturnable<Boolean> callbackInfo) {
+		if (callbackInfo.getReturnValue()) {
+			PowerHolderComponent.getPowers(player, MobNeutralityPower.class).forEach(power -> {
+				if (power.entityTypes.contains(EntityType.ENDERMAN) && power.isActive()) {
+					callbackInfo.setReturnValue(false);
+				}
+			});
+		}
+	}
+}
