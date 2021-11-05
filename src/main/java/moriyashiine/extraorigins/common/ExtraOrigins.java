99883803b1d26e5c6766d9dce055e80c6c88b539
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2021-11-05 13:11:15 -0500

    players with the mount power now dismount players before disconnecting

--- src/main/java/moriyashiine/extraorigins/common/ExtraOrigins.java
+++ src/main/java/moriyashiine/extraorigins/common/ExtraOrigins.java
@@ -1,12 +1,16 @@
 package moriyashiine.extraorigins.common;
 
+import io.github.apace100.apoli.component.PowerHolderComponent;
 import moriyashiine.extraorigins.common.network.packet.BoneMealPacket;
 import moriyashiine.extraorigins.common.network.packet.MountC2SPacket;
+import moriyashiine.extraorigins.common.power.MountPower;
 import moriyashiine.extraorigins.common.registry.EOConditions;
 import moriyashiine.extraorigins.common.registry.EOPowers;
 import moriyashiine.extraorigins.common.registry.EOScaleTypes;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class ExtraOrigins implements ModInitializer {
 	public static final String MOD_ID = "extraorigins";
@@ -18,5 +22,10 @@ public class ExtraOrigins implements ModInitializer {
 		EOConditions.init();
 		ServerPlayNetworking.registerGlobalReceiver(BoneMealPacket.ID, BoneMealPacket::receive);
 		ServerPlayNetworking.registerGlobalReceiver(MountC2SPacket.ID, MountC2SPacket::receive);
+		ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> {
+			if (PowerHolderComponent.hasPower(handler.player, MountPower.class) && handler.player.getVehicle() instanceof PlayerEntity) {
+				handler.player.dismountVehicle();
+			}
+		});
 	}
 }
