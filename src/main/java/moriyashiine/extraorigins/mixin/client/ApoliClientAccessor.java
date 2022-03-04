d03e7589dea8beb7c3e6f3ea07097bbc9ff0d10d
Pug <81167232+MerchantPug@users.noreply.github.com>
2022-03-04 21:27:22 +1100

    Set remap to false

--- src/main/java/moriyashiine/extraorigins/mixin/client/ApoliClientAccessor.java
+++ src/main/java/moriyashiine/extraorigins/mixin/client/ApoliClientAccessor.java
@@ -6,14 +6,13 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.option.KeyBinding;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 import java.util.HashMap;
 
 @Environment(EnvType.CLIENT)
 @Mixin(ApoliClient.class)
 public interface ApoliClientAccessor {
-    @Accessor("idToKeyBindingMap")
+    @Accessor(value = "idToKeyBindingMap", remap = false)
     static HashMap<String, KeyBinding> eo_getIdToKeybindingMap() {
         throw new RuntimeException("");
     }
