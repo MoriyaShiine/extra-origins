f4847ed9757b92c2141c670cf4be616778223e67
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2023-10-01 12:14:15 -0500

    readd modify_size scale type

--- src/main/java/moriyashiine/extraorigins/common/ExtraOrigins.java
+++ src/main/java/moriyashiine/extraorigins/common/ExtraOrigins.java
@@ -8,10 +8,7 @@ import io.github.apace100.apoli.component.PowerHolderComponent;
 import moriyashiine.extraorigins.common.packet.ChangeRadialDirectionPacket;
 import moriyashiine.extraorigins.common.packet.MountC2SPacket;
 import moriyashiine.extraorigins.common.power.MountPower;
-import moriyashiine.extraorigins.common.registry.ModConditions;
-import moriyashiine.extraorigins.common.registry.ModParticleTypes;
-import moriyashiine.extraorigins.common.registry.ModPowers;
-import moriyashiine.extraorigins.common.registry.ModSoundEvents;
+import moriyashiine.extraorigins.common.registry.*;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
@@ -27,6 +24,7 @@ public class ExtraOrigins implements ModInitializer {
 		ModSoundEvents.init();
 		ModPowers.init();
 		ModConditions.init();
+		ModScaleTypes.init();
 		initPackets();
 		initEvents();
 	}
---
+++ src/main/java/moriyashiine/extraorigins/common/registry/ModScaleTypes.java
@@ -0,0 +1,22 @@
+/*
+ * All Rights Reserved (c) MoriyaShiine
+ */
+
+package moriyashiine.extraorigins.common.registry;
+
+import moriyashiine.extraorigins.common.ExtraOrigins;
+import virtuoel.pehkui.api.*;
+
+public class ModScaleTypes {
+	public static ScaleType MODIFY_SIZE_TYPE;
+	public static ScaleModifier MODIFY_SIZE_MODIFIER;
+
+	public static void init() {
+		MODIFY_SIZE_MODIFIER = ScaleRegistries.register(ScaleRegistries.SCALE_MODIFIERS, ExtraOrigins.id("modify_size"), new TypedScaleModifier(() -> MODIFY_SIZE_TYPE));
+		ScaleTypes.WIDTH.getDefaultBaseValueModifiers().add(MODIFY_SIZE_MODIFIER);
+		ScaleTypes.HEIGHT.getDefaultBaseValueModifiers().add(MODIFY_SIZE_MODIFIER);
+		ScaleTypes.DROPS.getDefaultBaseValueModifiers().add(MODIFY_SIZE_MODIFIER);
+		ScaleTypes.VISIBILITY.getDefaultBaseValueModifiers().add(MODIFY_SIZE_MODIFIER);
+		MODIFY_SIZE_TYPE = ScaleRegistries.register(ScaleRegistries.SCALE_TYPES, ExtraOrigins.id("modify_size"), ScaleType.Builder.create().affectsDimensions().addDependentModifier(MODIFY_SIZE_MODIFIER).build());
+	}
+}
