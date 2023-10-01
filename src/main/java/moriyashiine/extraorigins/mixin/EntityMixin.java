3c5856a4d09b9a7047b8caf99eeda19e1566139b
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2023-10-01 12:42:35 -0500

    remove weird mount height calculations

--- src/main/java/moriyashiine/extraorigins/mixin/EntityMixin.java
+++ src/main/java/moriyashiine/extraorigins/mixin/EntityMixin.java
@@ -5,31 +5,17 @@
 package moriyashiine.extraorigins.mixin;
 
 import io.github.apace100.apoli.component.PowerHolderComponent;
-import moriyashiine.extraorigins.common.power.MountPower;
 import moriyashiine.extraorigins.common.power.PreventBlockSlownessPower;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Vec3d;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import virtuoel.pehkui.api.ScaleTypes;
 
 @Mixin(Entity.class)
-public abstract class EntityMixin {
-	@Shadow
-	private EntityDimensions dimensions;
-
-	@Shadow
-	@Nullable
-	public abstract Entity getFirstPassenger();
-
+public class EntityMixin {
 	@Inject(method = "slowMovement", at = @At("HEAD"), cancellable = true)
 	private void extraorigins$preventBlockSlowness(BlockState state, Vec3d multiplier, CallbackInfo ci) {
 		PowerHolderComponent.getPowers((Entity) (Object) this, PreventBlockSlownessPower.class).forEach(preventBlockSlownessPower -> {
@@ -37,12 +23,5 @@ public abstract class EntityMixin {
 				ci.cancel();
 			}
 		});
-	}
-
-	@Inject(method = "getMountedHeightOffset", at = @At("RETURN"), cancellable = true)
-	private void extraorigins$changeMountedPlayerOffsetWithPower(CallbackInfoReturnable<Double> cir) {
-		if ((Entity) (Object) this instanceof PlayerEntity player && getFirstPassenger() instanceof PlayerEntity rider && PowerHolderComponent.hasPower(rider, MountPower.class)) {
-			cir.setReturnValue(cir.getReturnValueD() * ScaleTypes.HEIGHT.getScaleData(player).getScale() / dimensions.height);
-		}
 	}
 }
