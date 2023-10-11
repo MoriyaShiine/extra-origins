b5f8b51c24e2c5c04a9018443dd76c550f123f20
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2023-10-11 11:57:34 -0500

    fix #214

--- src/main/java/moriyashiine/extraorigins/mixin/client/ProjectileUtilMixin.java
+++ src/main/java/moriyashiine/extraorigins/mixin/client/ProjectileUtilMixin.java
@@ -4,6 +4,8 @@
 
 package moriyashiine.extraorigins.mixin.client;
 
+import io.github.apace100.apoli.component.PowerHolderComponent;
+import moriyashiine.extraorigins.common.power.MountPower;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
@@ -20,6 +22,6 @@ import java.util.function.Predicate;
 public class ProjectileUtilMixin {
 	@ModifyVariable(method = "raycast", at = @At("HEAD"), argsOnly = true)
 	private static Predicate<Entity> extraorigins$preventHittingPlayerMount(Predicate<Entity> value) {
-		return value.and(entity -> entity.getRootVehicle() != MinecraftClient.getInstance().player);
+		return value.and(entity -> entity.getRootVehicle() != MinecraftClient.getInstance().player || !PowerHolderComponent.hasPower(entity, MountPower.class));
 	}
 }
