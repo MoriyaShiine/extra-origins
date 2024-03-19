b00b2c453419af44e7357fa66b3041cd7cb33af4
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-03-18 19:40:19 -0500

    piglin and inchling cleanup, code cleanup

--- src/main/java/moriyashiine/extraorigins/common/registry/ModConditions.java
+++ src/main/java/moriyashiine/extraorigins/common/registry/ModConditions.java
@@ -24,20 +24,20 @@ import net.minecraft.util.Pair;
 public class ModConditions {
 	public static final ConditionFactory<CachedBlockPosition> FERTILIZABLE = new ConditionFactory<>(ExtraOrigins.id("fertilizable"), new SerializableData(), (data, cachedBlockPosition) -> cachedBlockPosition.getBlockState().getBlock() instanceof Fertilizable fertilizable && fertilizable.isFertilizable(cachedBlockPosition.getWorld(), cachedBlockPosition.getBlockPos(), cachedBlockPosition.getBlockState()));
 
+	public static final ConditionFactory<Pair<DamageSource, Float>> SHOT_FROM_CROSSBOW = new ConditionFactory<>(ExtraOrigins.id("shot_from_crossbow"), new SerializableData(), (data, damageSourceFloatPair) -> damageSourceFloatPair.getLeft().getSource() instanceof PersistentProjectileEntity projectile && projectile.isShotFromCrossbow());
+
+	public static final ConditionFactory<Entity> PIGLIN_SAFE = new ConditionFactory<>(ExtraOrigins.id("piglin_safe"), new SerializableData(), (data, entity) -> entity.getWorld().getDimension().piglinSafe());
 	public static final ConditionFactory<Entity> RADIAL_MENU_DIRECTION = new ConditionFactory<>(ExtraOrigins.id("radial_menu_direction"), new SerializableData().add("direction", SerializableDataType.enumValue(RadialMenuDirection.class)).add("power", ApoliDataTypes.POWER_TYPE), (data, entity) -> {
 		if (((PowerTypeReference<?>) data.get("power")).get(entity) instanceof RadialMenuPower radialMenuPower) {
 			return radialMenuPower.isActive() && radialMenuPower.getDirection() == data.get("direction");
 		}
 		return false;
 	});
-	public static final ConditionFactory<Entity> PIGLIN_SAFE = new ConditionFactory<>(ExtraOrigins.id("piglin_safe"), new SerializableData(), (data, entity) -> entity.getWorld().getDimension().piglinSafe());
-
-	public static final ConditionFactory<Pair<DamageSource, Float>> SHOT_FROM_CROSSBOW = new ConditionFactory<>(ExtraOrigins.id("shot_from_crossbow"), new SerializableData(), (data, damageSourceFloatPair) -> damageSourceFloatPair.getLeft().getSource() instanceof PersistentProjectileEntity projectile && projectile.isShotFromCrossbow());
 
 	public static void init() {
 		Registry.register(ApoliRegistries.BLOCK_CONDITION, FERTILIZABLE.getSerializerId(), FERTILIZABLE);
-		Registry.register(ApoliRegistries.ENTITY_CONDITION, RADIAL_MENU_DIRECTION.getSerializerId(), RADIAL_MENU_DIRECTION);
-		Registry.register(ApoliRegistries.ENTITY_CONDITION, PIGLIN_SAFE.getSerializerId(), PIGLIN_SAFE);
 		Registry.register(ApoliRegistries.DAMAGE_CONDITION, SHOT_FROM_CROSSBOW.getSerializerId(), SHOT_FROM_CROSSBOW);
+		Registry.register(ApoliRegistries.ENTITY_CONDITION, PIGLIN_SAFE.getSerializerId(), PIGLIN_SAFE);
+		Registry.register(ApoliRegistries.ENTITY_CONDITION, RADIAL_MENU_DIRECTION.getSerializerId(), RADIAL_MENU_DIRECTION);
 	}
 }
--- src/main/java/moriyashiine/extraorigins/common/registry/ModPowers.java
+++ src/main/java/moriyashiine/extraorigins/common/registry/ModPowers.java
@@ -20,6 +20,14 @@ import net.minecraft.registry.Registry;
 import java.util.Collections;
 
 public class ModPowers {
+	public static final PowerFactory<?> CAN_WALK_ON_POWDER_SNOW = new PowerFactory<>(ExtraOrigins.id("can_walk_on_powder_snow"), new SerializableData(), data -> (type, entity) -> new CanStandOnPowderSnowPower(type, entity)).allowCondition();
+	public static final PowerFactory<?> FOOD_EFFECT_IMMUNITY = new PowerFactory<>(ExtraOrigins.id("food_effect_immunity"), new SerializableData().add("effects", SerializableDataTypes.STATUS_EFFECTS, Collections.emptyList()).add("inverted", SerializableDataTypes.BOOLEAN, false), data -> (type, entity) -> new FoodEffectImmunityPower(type, entity, data.get("effects"), data.getBoolean("inverted"))).allowCondition();
+	public static final PowerFactory<?> INNATE_UNBREAKING = new PowerFactory<>(ExtraOrigins.id("innate_unbreaking"), new SerializableData().add("level", SerializableDataTypes.INT).add("item_condition", ApoliDataTypes.ITEM_CONDITION, null), data -> (type, entity) -> new InnateUnbreakingPower(type, entity, data.getInt("level"), data.get("item_condition")));
+	public static final PowerFactory<?> MOB_NEUTRALITY = new PowerFactory<>(ExtraOrigins.id("mob_neutrality"), new SerializableData().add("entity_types", SerializableDataType.list(SerializableDataTypes.ENTITY_TYPE), Collections.emptyList()).add("inverted", SerializableDataTypes.BOOLEAN, false), data -> (type, entity) -> new MobNeutralityPower(type, entity, data.get("entity_types"), data.getBoolean("inverted"))).allowCondition();
+	public static final PowerFactory<?> MODIFY_AIR_STRAFING_SPEED = ValueModifyingPower.createValueModifyingFactory(ModifyAirStrafingSpeedPower::new, ExtraOrigins.id("modify_air_strafing_speed"));
+	public static final PowerFactory<?> MODIFY_ITEM_ATTRIBUTE = ModifyItemAttributePower.createFactory(ExtraOrigins.id("modify_item_attribute"));
+	public static final PowerFactory<?> MODIFY_SIZE = new PowerFactory<>(ExtraOrigins.id("modify_size"), new SerializableData().add("scale_types", SerializableDataTypes.IDENTIFIERS).add("scale", SerializableDataTypes.FLOAT), data -> (type, entity) -> new ModifySizePower(type, entity, data.get("scale_types"), data.getFloat("scale"))).allowCondition();
+	public static final PowerFactory<?> PREVENT_BLOCK_SLOWNESS = new PowerFactory<>(ExtraOrigins.id("prevent_block_slowness"), new SerializableData().add("blocks", SerializableDataType.list(SerializableDataTypes.BLOCK), Collections.emptyList()).add("inverted", SerializableDataTypes.BOOLEAN, false), data -> (type, entity) -> new PreventBlockSlownessPower(type, entity, data.get("blocks"), data.getBoolean("inverted"))).allowCondition();
 	public static final PowerFactory<?> RADIAL_MENU = new PowerFactory<>(ExtraOrigins.id("radial_menu"), new SerializableData()
 			.add("key", ApoliDataTypes.KEY, new Active.Key())
 			.add("sprite_location", SerializableDataTypes.IDENTIFIER)
@@ -34,39 +42,22 @@ public class ModPowers {
 		power.setKey(data.get("key"));
 		return power;
 	}).allowCondition();
-
-	public static final PowerFactory<?> FOOD_EFFECT_IMMUNITY = new PowerFactory<>(ExtraOrigins.id("food_effect_immunity"), new SerializableData().add("effects", SerializableDataTypes.STATUS_EFFECTS, Collections.emptyList()).add("inverted", SerializableDataTypes.BOOLEAN, false), data -> (type, entity) -> new FoodEffectImmunityPower(type, entity, data.get("effects"), data.getBoolean("inverted"))).allowCondition();
-
-	public static final PowerFactory<?> MODIFY_SIZE = new PowerFactory<>(ExtraOrigins.id("modify_size"), new SerializableData().add("scale_types", SerializableDataTypes.IDENTIFIERS).add("scale", SerializableDataTypes.FLOAT), data -> (type, entity) -> new ModifySizePower(type, entity, data.get("scale_types"), data.getFloat("scale"))).allowCondition();
-
-	public static final PowerFactory<?> PREVENT_BLOCK_SLOWNESS = new PowerFactory<>(ExtraOrigins.id("prevent_block_slowness"), new SerializableData().add("blocks", SerializableDataType.list(SerializableDataTypes.BLOCK), Collections.emptyList()).add("inverted", SerializableDataTypes.BOOLEAN, false), data -> (type, entity) -> new PreventBlockSlownessPower(type, entity, data.get("blocks"), data.getBoolean("inverted"))).allowCondition();
-
-	public static final PowerFactory<?> CAN_WALK_ON_POWDER_SNOW = new PowerFactory<>(ExtraOrigins.id("can_walk_on_powder_snow"), new SerializableData(), data -> (type, entity) -> new CanStandOnPowderSnowPower(type, entity)).allowCondition();
-
 	public static final PowerFactory<?> START_RIDING = new PowerFactory<>(ExtraOrigins.id("start_riding"), new SerializableData().add("key", ApoliDataTypes.KEY, new Active.Key()), data -> (type, entity) -> {
 		MountPower power = new MountPower(type, entity);
 		power.setKey(data.get("key"));
 		return power;
 	}).allowCondition();
 
-	public static final PowerFactory<?> MOB_NEUTRALITY = new PowerFactory<>(ExtraOrigins.id("mob_neutrality"), new SerializableData().add("entity_types", SerializableDataType.list(SerializableDataTypes.ENTITY_TYPE), Collections.emptyList()).add("inverted", SerializableDataTypes.BOOLEAN, false), data -> (type, entity) -> new MobNeutralityPower(type, entity, data.get("entity_types"), data.getBoolean("inverted"))).allowCondition();
-
-	public static final PowerFactory<?> MODIFY_AIR_STRAFING_SPEED = ValueModifyingPower.createValueModifyingFactory(ModifyAirStrafingSpeedPower::new, ExtraOrigins.id("modify_air_strafing_speed"));
-
-	public static final PowerFactory<?> INNATE_UNBREAKING = new PowerFactory<>(ExtraOrigins.id("innate_unbreaking"), new SerializableData().add("level", SerializableDataTypes.INT).add("item_condition", ApoliDataTypes.ITEM_CONDITION, null), data -> (type, entity) -> new InnateUnbreakingPower(type, entity, data.getInt("level"), data.get("item_condition")));
-
-	public static final PowerFactory<?> MODIFY_ITEM_ATTRIBUTE = ModifyItemAttributePower.createFactory(ExtraOrigins.id("modify_item_attribute"));
-
 	public static void init() {
-		Registry.register(ApoliRegistries.POWER_FACTORY, RADIAL_MENU.getSerializerId(), RADIAL_MENU);
-		Registry.register(ApoliRegistries.POWER_FACTORY, FOOD_EFFECT_IMMUNITY.getSerializerId(), FOOD_EFFECT_IMMUNITY);
-		Registry.register(ApoliRegistries.POWER_FACTORY, MODIFY_SIZE.getSerializerId(), MODIFY_SIZE);
-		Registry.register(ApoliRegistries.POWER_FACTORY, PREVENT_BLOCK_SLOWNESS.getSerializerId(), PREVENT_BLOCK_SLOWNESS);
 		Registry.register(ApoliRegistries.POWER_FACTORY, CAN_WALK_ON_POWDER_SNOW.getSerializerId(), CAN_WALK_ON_POWDER_SNOW);
-		Registry.register(ApoliRegistries.POWER_FACTORY, START_RIDING.getSerializerId(), START_RIDING);
+		Registry.register(ApoliRegistries.POWER_FACTORY, FOOD_EFFECT_IMMUNITY.getSerializerId(), FOOD_EFFECT_IMMUNITY);
+		Registry.register(ApoliRegistries.POWER_FACTORY, INNATE_UNBREAKING.getSerializerId(), INNATE_UNBREAKING);
 		Registry.register(ApoliRegistries.POWER_FACTORY, MOB_NEUTRALITY.getSerializerId(), MOB_NEUTRALITY);
 		Registry.register(ApoliRegistries.POWER_FACTORY, MODIFY_AIR_STRAFING_SPEED.getSerializerId(), MODIFY_AIR_STRAFING_SPEED);
-		Registry.register(ApoliRegistries.POWER_FACTORY, INNATE_UNBREAKING.getSerializerId(), INNATE_UNBREAKING);
 		Registry.register(ApoliRegistries.POWER_FACTORY, MODIFY_ITEM_ATTRIBUTE.getSerializerId(), MODIFY_ITEM_ATTRIBUTE);
+		Registry.register(ApoliRegistries.POWER_FACTORY, MODIFY_SIZE.getSerializerId(), MODIFY_SIZE);
+		Registry.register(ApoliRegistries.POWER_FACTORY, PREVENT_BLOCK_SLOWNESS.getSerializerId(), PREVENT_BLOCK_SLOWNESS);
+		Registry.register(ApoliRegistries.POWER_FACTORY, RADIAL_MENU.getSerializerId(), RADIAL_MENU);
+		Registry.register(ApoliRegistries.POWER_FACTORY, START_RIDING.getSerializerId(), START_RIDING);
 	}
 }
